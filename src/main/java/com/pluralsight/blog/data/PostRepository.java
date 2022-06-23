744280e466907b42f1cfce1c16419fdee2b1236e
Tuan-Anh BUI <tuananh.hufs@gmail.com>
2022-06-23 22:50:58 +0200

    First lines of code for PostRepository

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,29 +1,6 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
-import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import com.pluralsight.blog.model.Post;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
-
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
-
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
-    }
-}
+public interface PostRepository extends JpaRepository<Post, Long> {}
