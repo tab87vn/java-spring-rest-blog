11a04e86beb2ee8927035fde6c1aeb2d7937b373
Tuan-Anh BUI <tuananh.hufs@gmail.com>
2022-06-23 23:32:01 +0200

    Add find by title method

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,6 +1,14 @@
 package com.pluralsight.blog.data;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import com.pluralsight.blog.model.Post;
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long>
+{
+    @RestResource(rel = "contains-title", path = "containsTitle")
+    List<Post> findByTitleContaining(String title);
+
+}
